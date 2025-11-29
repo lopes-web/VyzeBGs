a4a5f528d76f5b8906198ef21b538a8175da1e9b
Lopes Web <lopesweb49@gmail.com>
2025-11-29 01:51:49 -0300

    Fix blank screen: Restore App component in index.tsx

--- index.tsx
+++ index.tsx
@@ -6,5 +6,8 @@ import { AuthProvider } from './components/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
